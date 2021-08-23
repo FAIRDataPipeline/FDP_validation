7d2500b7dfc43d195321503e99e108bd3c13510f
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-08-24 00:30:25 +0100

    download working configs

--- scripts/examples.jl
+++ scripts/examples.jl
@@ -7,7 +7,7 @@ import CSV
 import DataFrames
 import Random
 
-parent_dir = "/Users/Soniam/Desktop/coderun/"
+parent_dir = "coderun/"
 dirs = filter(x -> isdir(joinpath(parent_dir, x)), readdir(parent_dir, join = true))
 
 ## 1. Empty code run
