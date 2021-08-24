42a94c6ae8b206825abb584f096d979f84fd4786
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-08-24 10:40:33 +0100

    try to download to working dir

--- scripts/examples.jl
+++ scripts/examples.jl
@@ -7,7 +7,7 @@ import CSV
 import DataFrames
 import Random
 
-parent_dir = "/Users/runner/work/FDP_validation/FDP_validation/coderun/"
+parent_dir = "working-configs"
 dirs = filter(x -> isdir(joinpath(parent_dir, x)), readdir(parent_dir, join = true))
 
 ## 1. Empty code run
