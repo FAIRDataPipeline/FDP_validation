367f03e43defdab289f8e0b1fbacc94a4e3ac3d3
soniamitchell <sonia.mitchell@glasgow.ac.uk>
2021-08-31 13:50:42 +0100

    update package name

--- scripts/examples.jl
+++ scripts/examples.jl
@@ -1,8 +1,8 @@
-import Pkg; Pkg.add("DataRegistryUtils")
+import Pkg; Pkg.add("DataPipeline")
 Pkg.add("CSV")
 Pkg.add("DataFrames")
 Pkg.add("Random")
-import DataRegistryUtils
+import DataPipeline
 import CSV
 import DataFrames
 import Random
@@ -14,14 +14,14 @@ println(dirs)
 
 ## 1. Empty code run
 wc = readdir(dirs[1], join = true)[2]
-handle = DataRegistryUtils.initialise(wc)
-DataRegistryUtils.finalise(handle; comments="Empty code run example.")
+handle = DataPipeline.initialise(wc)
+DataPipeline.finalise(handle; comments="Empty code run example.")
 
 ## 4. Write data product (csv)
 wc = readdir(dirs[2], join = true)[2]
-handle = DataRegistryUtils.initialise(wc)
+handle = DataPipeline.initialise(wc)
 # println(typeof(handle.config["write"][1]["use"]["public"]))
 # println(typeof(handle.config["write"][1]["use"]["public2"]))
 tmp = CSV.read("examples/register/tbl.csv", DataFrames.DataFrame)
-DataRegistryUtils.write_table(handle, tmp, "test/csv")
-DataRegistryUtils.finalise(handle; comments="Write CSV example.")
+DataPipeline.write_table(handle, tmp, "test/csv")
+DataPipeline.finalise(handle; comments="Write CSV example.")
